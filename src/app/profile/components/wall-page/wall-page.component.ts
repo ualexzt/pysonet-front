3bf24affcc0dae447a26723024f520901dc2358f
ualexzt <ualexzt@gmail.com>
2021-04-01 13:30:53 +0300

    add wall-page in project

---
+++ src/app/profile/components/wall-page/wall-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wall-page',
+  templateUrl: './wall-page.component.html',
+  styleUrls: ['./wall-page.component.scss']
+})
+export class WallPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
