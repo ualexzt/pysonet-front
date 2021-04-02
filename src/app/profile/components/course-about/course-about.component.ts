54894463424a090096b0f9e31cd19019d35b5b65
ualexzt <ualexzt@gmail.com>
2021-04-02 19:16:24 +0300

    add course-about page

---
+++ src/app/profile/components/course-about/course-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-about',
+  templateUrl: './course-about.component.html',
+  styleUrls: ['./course-about.component.scss']
+})
+export class CourseAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
